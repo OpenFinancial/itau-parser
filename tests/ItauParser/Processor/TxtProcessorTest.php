0ce173f1a5e48c6f0af6d008aca5fb474cba1cb1
Daniel Costa <danielcosta@gmail.com>
2014-05-19 09:07:27 -0300

    Initial unit tests, Travis CI and composer.json enhancements
    - Added initial unit tests
    - Added Travis CI support
    - Enhanced composer.json with phpunit dependency for dev and autoload statement

---
+++ tests/ItauParser/Processor/TxtProcessorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ItauParser\Processor;
+
+use ItauParser\Parser;
+use ItauParser\Transaction\Collection;
+use ItauParser\Transaction\DebitTransaction;
+
+class TxtProcessorTest extends \PHPUnit_Framework_TestCase {
+    public function testProcess()
+    {
+        $data = "17/02/2014;RSHOP-ASDQWEA    -15/02 ;-100,00\n17/02/2014;RSHOP-EWQQ DDD   -14/02 ;-50,00";
+        $parser = new Parser(new TxtProcessor($data));
+
+        $collection = new Collection();
+
+        $transaction = new DebitTransaction();
+        $transaction->setDateEffected(\DateTime::createFromFormat('m-d', '02-15'));
+        $transaction->setDate(\DateTime::createFromFormat('m-d', '02-17'));
+        $transaction->setAmount(-100);
+        $transaction->setDescription('ASDQWEA    ');
+        $transaction->setAmountType('debit');
+        $collection->add($transaction);
+
+        $transaction = new DebitTransaction();
+        $transaction->setDateEffected(\DateTime::createFromFormat('m-d', '02-14'));
+        $transaction->setDate(\DateTime::createFromFormat('m-d', '02-17'));
+        $transaction->setAmount(-50);
+        $transaction->setDescription('EWQQ DDD   ');
+        $transaction->setAmountType('debit');
+        $collection->add($transaction);
+
+        $this->assertEquals($collection, $parser->parse());
+    }
+}
+ 
---
+++ tests/ItauParser/Transaction/TransferTransactionTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ItauParser\Transaction;
+
+class TransferTransactionTest extends \PHPUnit_Framework_TestCase {
+    public function testAccountHolder()
+    {
+        $transfer = new TransferTransaction();
+        $transfer->setAccountHolder('Account Holder');
+        $this->assertEquals($transfer->getAccountHolder(), 'Account Holder');
+    }
+
+    public function testAccountNumber()
+    {
+        $transfer = new TransferTransaction();
+        $transfer->setAccountNumber('1254323');
+        $this->assertEquals($transfer->getAccountNumber(), '1254323');
+    }
+
+    public function testAccountType()
+    {
+        $transfer = new TransferTransaction();
+        $transfer->setAccountType('Personal Type');
+        $this->assertEquals($transfer->getAccountType(), 'Personal Type');
+    }
+
+    public function testBankNumber()
+    {
+        $transfer = new TransferTransaction();
+        $transfer->setBankNumber('1254323');
+        $this->assertEquals($transfer->getBankNumber(), '1254323');
+    }
+
+    public function testRoutingNumber()
+    {
+        $transfer = new TransferTransaction();
+        $transfer->setRoutingNumber(125432);
+        $this->assertEquals($transfer->getRoutingNumber(), 125432);
+    }
+
+    public function testTransferType()
+    {
+        $transfer = new TransferTransaction();
+        $transfer->setTransferType('transfer type');
+        $this->assertEquals($transfer->getTransferType(), 'transfer type');
+    }
+
+    public function testMatchTransferTypeDoc()
+    {
+        $this->assertEquals(TransferTransaction::TRANSFER_TYPE_DOC, 'DOC');
+        $this->assertEquals(TransferTransaction::TRANSFER_TYPE_TBI, 'TBI');
+        $this->assertEquals(TransferTransaction::TRANSFER_TYPE_TED, 'TED');
+    }
+}
+ 
